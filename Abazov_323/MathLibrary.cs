3f664ebedb09df7f7e6147a7408ee48836d24f85
235002 <235002@KB37-120-C02>
2026-03-19 10:45:07 +0300

    Практическая работа 6.2

---
+++ Abazov_323/MathLibrary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Abazov_323
+{
+    public class Core
+    {
+        public static BankDBEntities Context = new BankDBEntities();
+    }
+
+
+    public class MathLibrary
+    {
+
+        /// <param name="x">Значение X.</param>
+        /// <param name="y">Значение Y.</param>
+        /// <param name="z">Значение Z.</param>
+        /// <returns>Результат вычисления.</returns>
+        public double CalculateComplexExpression(double x, double y, double z)
+        {
+            // ЗАМЕНЕНО НА ArgumentOutOfRangeException, чтобы тесты не падали
+            if (Math.Abs(x - 2) < 0.000001) throw new ArgumentOutOfRangeException("x", "Division by zero in sqrt");
+            if (Math.Atan(z) == 0) throw new ArgumentOutOfRangeException("z", "Division by zero in atan");
+
+            double part1 = (Math.Exp(x) + 1) / Math.Sqrt(Math.Abs(x - 2));
+            double part2 = (2 * Math.Abs(x + y) * Math.Pow(x + 1, -1)) / Math.Atan(z);
+            return part1 * part2;
+        }
+
+        /// <summary>
+        /// Вычисляет значение с условием выбора функции (Page2).
+        /// </summary>
+        /// <param name="x">Аргумент x.</param>
+        /// <param name="y">Аргумент y.</param>
+        /// <param name="mode">Режим: 1 - sinh, 2 - sqr, остальное - exp.</param>
+        /// <returns>Результат d.</returns>
+        public double CalculateConditional(double x, double y, int mode)
+        {
+            double fx;
+            if (mode == 1) fx = Math.Sinh(x);
+            else if (mode == 2) fx = Math.Pow(x, 2);
+            else fx = Math.Exp(x);
+
+            if (x > y) return Math.Pow(fx - y, 3) + Math.Atan(fx);
+            else if (y > x) return Math.Pow(y - fx, 3) + Math.Atan(fx);
+            else return Math.Pow(y + fx, 3) + 0.5;
+        }
+
+        /// <summary>
+        /// Вычисляет значение функции для табулирования (Page3).
+        /// </summary>
+        /// <param name="x">Аргумент x.</param>
+        /// <param name="b">Параметр b.</param>
+        /// <returns>Значение y.</returns>
+        public double CalculateTabulation(double x, double b)
+        {
+            if (x < 0) throw new ArgumentOutOfRangeException("x", "x must be >= 0 for Pow(x, 2.5)");
+            return (Math.Pow(x, 2.5) - b) * Math.Log(x * x + 12.7);
+        }
+    }
+
+    public class BankDBEntities { }
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Abazov_323;
+using System;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class MathTests
+    {
+        private MathLibrary math = new MathLibrary();
+
+        // --- PAGE 1: 5 тестов ---
+        [TestMethod]
+        public void P1_Complex_ValidData_ReturnsValue() => Assert.AreEqual(0, math.CalculateComplexExpression(0, 0, 1), 0.001);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void P1_Complex_XEqualsTwo_ThrowsException() => math.CalculateComplexExpression(2, 0, 1);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void P1_Complex_ZZero_ThrowsException() => math.CalculateComplexExpression(0, 0, 0);
+
+        [TestMethod]
+        public void P1_Complex_NegativeX_ReturnsValue() => Assert.IsNotNull(math.CalculateComplexExpression(-5, 2, 0.5));
+
+        [TestMethod]
+        public void P1_Complex_LargeValues_ReturnsValue() => Assert.IsNotNull(math.CalculateComplexExpression(10, 10, 10));
+
+        // --- PAGE 2: 6 тестов ---
+        [TestMethod]
+        public void P2_Cond_XMoreY_Sinh_ReturnsValue() => Assert.IsNotNull(math.CalculateConditional(5, 2, 1));
+
+        [TestMethod]
+        public void P2_Cond_YMoreX_Sqr_ReturnsValue() => Assert.IsNotNull(math.CalculateConditional(2, 5, 2));
+
+        [TestMethod]
+        public void P2_Cond_XEqualY_Exp_ReturnsValue() => Assert.IsNotNull(math.CalculateConditional(1, 1, 3));
+
+        [TestMethod]
+        public void P2_Cond_ZeroValues_ReturnsValue() => Assert.IsNotNull(math.CalculateConditional(0, 0, 2));
+
+        [TestMethod]
+        public void P2_Cond_NegativeX_ReturnsValue() => Assert.IsNotNull(math.CalculateConditional(-1, 2, 1));
+
+        [TestMethod]
+        public void P2_Cond_ModeDefault_UsesExp() => Assert.IsNotNull(math.CalculateConditional(1, 2, 99));
+
+        // --- PAGE 3: 5 тестов ---
+        [TestMethod]
+        public void P3_Tab_ValidData_ReturnsValue() => Assert.IsNotNull(math.CalculateTabulation(2, 1));
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void P3_Tab_NegativeX_ThrowsException() => math.CalculateTabulation(-1, 1);
+
+        [TestMethod]
+        public void P3_Tab_XZero_ReturnsValue() => Assert.AreEqual(-1 * Math.Log(12.7), math.CalculateTabulation(0, 1), 0.001);
+
+        [TestMethod]
+        public void P3_Tab_BZero_ReturnsValue() => Assert.IsNotNull(math.CalculateTabulation(1, 0));
+
+        [TestMethod]
+        public void P3_Tab_LargeX_ReturnsValue() => Assert.IsNotNull(math.CalculateTabulation(100, 5));
+    }
+}
